2562a40dc3fd12aa4e47f5ac95746f5ae88b63c9
Tomasz Milczarek <miltomasz@mac>
2013-05-25 21:16:15 +0200

    rails admin initializer hashed

--- config/initializers/rails_admin.rb
+++ config/initializers/rails_admin.rb
@@ -7,12 +7,12 @@
   ################  Global configuration  ################
 
   # Set the admin name here (optional second array element will appear in red). For example:
-  config.main_app_name = ['Gjk', 'Admin']
+  #      config.main_app_name = ['Gjk', 'Admin']
   # or for a more dynamic name:
   # config.main_app_name = Proc.new { |controller| [Rails.application.engine_name.titleize, controller.params['action'].titleize] }
 
   # RailsAdmin may need a way to know who the current user is]
-  config.current_user_method { current_gjk_user } # auto-generated
+  #      config.current_user_method { current_gjk_user } # auto-generated
 
   # If you want to track changes on your models:
   # config.audit_with :history, 'GjkUser'
